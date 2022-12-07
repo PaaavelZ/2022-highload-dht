e20c98d9a6c54842292c88611fb153952420fcbe
PaaavelZ <paaavel.2024@gmail.com>
2022-12-07 15:56:32 +0300

    JAR

--- src/main/java/ok/dht/test/kovalenko/utils/Main.java
+++ src/main/java/ok/dht/test/kovalenko/utils/Main.java
@@ -16,38 +16,16 @@ public final class Main {
     private static final Logger log = LoggerFactory.getLogger(Main.class);
     private static final List<Integer> ports = List.of(19234, 19235, 19236);
     private static final List<String> urls = ports.stream().map(p -> "http://localhost:" + p).toList();
-    private static final List<ServiceConfig> configs = new ArrayList<>(urls.size());
-
-    static {
-        for (int i = 0; i < urls.size(); ++i) {
-            try {
-                int port = ports.get(i);
-                String url = urls.get(i);
-                java.nio.file.Path cfgPath
-                        = java.nio.file.Path.of("/home/pavel/IntelliJIdeaProjects/tables/shard" + (i + 1));
-                Files.createDirectories(cfgPath);
-                ServiceConfig cfg = new ServiceConfig(
-                        port,
-                        url,
-                        urls,
-                        cfgPath
-                );
-                configs.add(cfg);
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
-        }
-    }
 
     private Main() {
     }
 
     public static void main(String[] args) {
         try {
-            int port = Integer.parseInt(args[0]);
-            String url = args[1];
-            List<String> clusterUrls = List.of(args[2].split(", "));
-            Path workingDir = Path.of(args[3]);
+            String url = args[0];
+            int port = Integer.parseInt(url.split(":")[1]);
+            List<String> clusterUrls = List.of(args[1].split(", "));
+            Path workingDir = Path.of(args[2]);
             ServiceConfig cfg = new ServiceConfig(port, url, clusterUrls, workingDir);
             MyServiceBase service = new MyServiceBase(cfg);
             service.start().get(1, TimeUnit.SECONDS);
@@ -61,7 +39,16 @@ public final class Main {
 
     public static void main(int serviceOrdinal) {
         try {
-            MyServiceBase service = new MyServiceBase(configs.get(serviceOrdinal - 1));
+            int port = ports.get(serviceOrdinal - 1);
+            String url = urls.get(serviceOrdinal - 1);
+            java.nio.file.Path cfgPath = Files.createTempDirectory("shards").resolve("shard" + serviceOrdinal);
+            ServiceConfig cfg = new ServiceConfig(
+                    port,
+                    url,
+                    urls,
+                    cfgPath
+            );
+            MyServiceBase service = new MyServiceBase(cfg);
             service.start().get(1, TimeUnit.SECONDS);
         } catch (Exception e) {
             log.error("Socket wasn't started: {}", urls.get(serviceOrdinal - 1), e);
